c5b3406b974c048cafbe12687b741569cbc89e3e
AKA-Yi <1437936463@qq.com>
2023-03-25 00:08:37 +0800

    Site updated: 2023-03-25 00:08:33

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "120",
+    "background": "5",
+    "tmpColor": "000000",
+    "tmpSize": "16",
+    "cityColor": "000000",
+    "citySize": "16",
+    "aqiColor": "000000",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "accb09973d1a488288daf7b5ea4f702a"
+  }
+}
